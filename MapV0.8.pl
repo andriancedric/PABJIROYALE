900dea8d5a13ee8a096b88520fd3c9872866503c
anakduaribu <andriancedric@gmail.com>
2018-11-24 17:04:30 +0700

    Update MapV0.8.pl

--- MapV0.8.pl
+++ MapV0.8.pl
@@ -1,17 +1,16 @@
 :- dynamic(gamemap/1).
 :- dynamic(deathclock/1). deathclock(0).
 :- dynamic(threatlvl/1). threatlvl(0).
-:- dynamic(player_health/1). player_health(100).
 :- dynamic(player_position/2).  player_position(2,2).
 :- dynamic(player_weapon/1). player_weapon(watergun).
+:- dynamic(player_bag/2).  player_bag(5,[]).
 :- dynamic(mapEff_row/1). mapEff_row(10).
 :- dynamic(mapEff_col/1). mapEff_col(10).
 :- dynamic(map_row/1). map_row(12).
 :- dynamic(map_col/1). map_col(12).
-:- dynamic(turn/1). turn(1).
 
 :-dynamic(enemy/2). % enemy(Type, HP, Weapon, X, Y)
-:-dynamic(enemynumber/1). enemynumber(1).
+:-dynamic(enemynumber/1). enemynumber(0).
 
 :- dynamic(minimaps/1). minimaps([[[]]]).
 :- dynamic(gamemap/1).
@@ -29,6 +28,17 @@ gamemap([
     ['X','-','-','-','-','-','-','-','-','-','-','X'],
     ['X','X','X','X','X','X','X','X','X','X','X','X']
     ]).
+
+/*Object*/
+init_object :-
+    	asserta(object(weapon,gun,3,3)),
+    	asserta(object(weapon,pan,4,4)),
+    	asserta(object(weapon,ak47,3,5)),
+    	asserta(object(armor,light,4,5)),
+    	asserta(object(armor,heavy,6,2)),
+    	asserta(object(medicine,tolakangin,5,5)),
+    	asserta(object(medicine,panadol,4,3)),
+    	asserta(object(medicine,aspirin,5,6)).
 
 %SWI-GNU PROLOG COMPATIBILITY RULES :----------------------------------
 
@@ -246,9 +256,9 @@ gamemap([
     at(Mat, Row, Col, Val) :- nth1(Row, Mat, ARow), nth1(Col, ARow, Val).
 
   %BOOM BOOM AREA (ADVANCES DEATHZONE BY 1 INCREMENT) -----------------------
-    %expandDZ(threatlvl)
+    %youdie(threatlvl)
 
-    expandDZ :- threatlvl(X),
+    youdie :- threatlvl(X),
                 NX is X+1, gamemap(M),
                 replace_row(M,(1+NX),'X',M2),
                 makecolX(M2,(1+NX),M3),
@@ -267,9 +277,9 @@ gamemap([
       %repeat :- repeat.
 
       placeValidity(X,Y,Z) :- gamemap(M), at(M,X,Y,Val), Val == Z.
-	
+
 	enemyTypeList(['S','M','L']).
-	
+
     placeRandomEnemy :- map_row(R), map_col(C), threatlvl(LVL),
                           Low is 1+LVL, High is R-LVL,
                           Low2 is 2+LVL, High2 is C-LVL,
@@ -279,7 +289,7 @@ gamemap([
                           random(Low2, High2, Y),
                           ( (moveValidity(X,Y,'-'))  ->
                             fail;
-						  (	
+						  (
 							enemynumber(N), Newnumber is N+1, retract(enemynumber(N)), asserta(enemynumber(Newnumber)),
 							  (
 								randomize,
@@ -297,20 +307,17 @@ gamemap([
 
 	%RANDOM PLAYER PLACEMENT GROUP ----------------------------------------------
 
-		placeRandomPlayer :- map_row(R), map_col(C), threatlvl(LVL),
-							  Low is 1+LVL, High is R-LVL,
-							  Low2 is 2+LVL, High2 is C-LVL,
-							  repeat,
-							  randomize, %USE RANDOMIZE TO RESET SEED.
-							  random(Low, High, X),
-							  random(Low2, High2, Y),
-							  ( (moveValidity(X,Y,'-'))  ->
-								fail;
-							  ( changemap(_,X,Y,'P',M2), retract(player_position(_,_)), asserta(player_position(X,Y)) , retractall(gamemap(_)), asserta(gamemap(M2)))).
-	
-	%RANDOM PLAYER PLACEMENT GROUP ----------------------------------------------
-	
-		%placeRandomItem :- 
+	placeRandomPlayer :- map_row(R), map_col(C), threatlvl(LVL),
+                          Low is 1+LVL, High is R-LVL,
+                          Low2 is 2+LVL, High2 is C-LVL,
+                          repeat,
+                          randomize, %USE RANDOMIZE TO RESET SEED.
+                          random(Low, High, X),
+                          random(Low2, High2, Y),
+                          ( (moveValidity(X,Y,'-'))  ->
+                            fail;
+                          ( changemap(_,X,Y,'P',M2), retract(player_position(_,_)), asserta(player_position(X,Y)) , retractall(gamemap(_)), asserta(gamemap(M2)))).
+
   %MOVEMENT GROUP ----------------------------------------------
     %MOVE PLAYER :
         moveValidity(X,Y,Z) :- gamemap(M), at(M,X,Y,Val), Val \= Z.
@@ -394,12 +401,12 @@ gamemap([
 
     dealDMG(HP, W, HP2) :- ((W = 'watergun') -> (randomize, random(1,10,DMG), HP2 is HP-DMG) ; true),
                            ((W = 'YoYo') -> (randomize, random(1,21,DMG), HP2 is HP-DMG); true),
-                           ((W = 'Da Piss Tall') -> (randomize, random(50,100,DMG), HP2 is HP-DMG) ; true).
+                           ((W = 'Da Piss Tall') -> (randomize, random(1,100,DMG), HP2 is HP-DMG) ; true).
 
     attack :- player_weapon(W), player_position(X,Y), positionmatchES(X,Y), enemy(T,HP,W,X,Y) , dealDMG(HP,W,HP2), write('Leftover enemy HP : '), write(HP2),
               enemynumber(N),
               (
-				  (HP2 < 1) -> ( NewN is N-1,retract(enemy(T,HP,W,X,Y)), retract(enemynumber(N)), asserta(enemynumber(NewN)), write('ENEMY DEAD'),nl ) 
+				  (HP2 =< 0) -> ( NewN is N-1,retract(enemy(T,HP,W,X,Y)), retract(enemynumber(N)), asserta(enemynumber(NewN)), write('ENEMY DEAD'),nl )
 								;
 								(retract(enemy(T,HP,W,X,Y)), asserta(enemy(T,HP2,W,X,Y)), write('ENEMY STILL ALIVE'),nl )
 			   ).
@@ -415,30 +422,73 @@ gamemap([
 		game :-
 			  write('>> '),
         %call(enemyRandomMove),
-			  read(Command),
-			  turn(T),
-			  /*Advance Turn Counter :*/
-				( (Command = map ; Command= look ; Command= status) -> true ; 
-					( NewT is T+1, retract(turn(T)), asserta(turn(NewT)),(
-							/*Check Deadzone Expansion :*/
-							( (NewT mod 10 =:= 0) -> expandDZ ; true),  
-							/*Check DMG :*/
-							( (player_position(X,Y), positionmatchES(X,Y), enemy(Type,_,_,X,Y) ) -> (player_health(HP), 
-								((Type == 'S') -> (randomize, random(1,5,DMG), HP2 is HP-DMG) ; true),
-								((Type == 'M')-> (randomize, random(3,10,DMG), HP2 is HP-DMG); true),
-								((Type == 'L') -> (randomize, random(15,30,DMG), HP2 is HP-DMG) ; true),
-								retract(player_health(HP)), asserta(player_health(HP2)), write('Current HP : '), write(HP2), nl,
-								/*CHECK GAMEOVER */
-								( ( HP2 < 1 ) -> (write('YOU DIED!'), halt) ; true)
-								); true
-							)
-						);true
-					)
-				),
-			  /*Call Command :*/
-				(call(Command) -> true ; game),
-			  
-			  /*Check EXIT command : */
-				((Command = end) -> halt ; game).
+			  read(X),
+			  (call(X) -> true ; game),
+			  ((X = end) -> halt ; game).
 
-		initgame :- setup, game.
+initgame :- init_object, setup, game.
+
+% TAKE & USE : --------------------------------------------------------------------------------------------
+
+take(X) :- (object(_,X,X1,Y1)),
+           player_bag(Size,Inv),
+           length(Inv,C),
+           C < Size,
+           positionmatchI(X1,Y1),
+           append(Inv,[X],New_inv),
+           retract(player_bag(Size,Inv)),
+           asserta(player_bag(Size,New_inv)),
+           write('Take success'),nl,
+           print_Inv(New_inv),!.
+
+take(X) :- (object(_,X,_,_)),
+           player_bag(Size,Inv),
+           length(Inv,C),
+           C < Size,
+           format("~p not on the ground",[X]),nl,!.
+
+take(_) :- player_bag(Size,Inv),
+           length(Inv,C),
+           C is Size,
+           write('Bag Full'),nl,!.
+
+print_Inv([]) :- write('Inv Empty'),nl,!.
+print_Inv([X|[]]) :- format("~p",[X]),nl,!.
+print_Inv([X|Y]) :- format("~p,",[X]),print_Inv(Y),!.
+
+use(X) :- object(_,X,_,_),
+          player_bag(Size,Inv),
+          member(X,Inv),
+          select(X,Inv,New_inv),
+          retract(player_bag(Size,Inv)),
+          asserta(player_bag(Size,New_inv)),
+          format("~p used",[X]),nl,
+          print_Inv(New_inv),!.
+
+use(X) :- player_bag(_,Inv),
+          format("~p is not in your bag",[X]),nl,
+          print_Inv(Inv),!.
+
+positionmatchI(X1,Y1) :- player_position(X,Y), (X == X1, Y == Y1) ,!.
+
+%RULE CERITA : -------------------------------------
+  enemy_north:- write('You see someone is moving. DAMN! He is in front of you. Attack him or you die?'), nl.
+  enemy_south:- write('Do you hear that? Someone is following you. Oh no! He is behind you. Grab your weapon, now!'), nl.
+  enemy_west:- write('Men always left because women always right. Is it true? Look at left and you see your enemy. Oops.'), nl.
+  enemy_east:- write('Left. Right. Left. RIGHT! He is coming. Be a man, finish him!'), nl.
+  your_weapon :- write('Your weapon is ...'), player_weapon(X), write(X), nl.
+  look_weapon :- write('Hey. No weapon, huh? Look around. You see a weapon. Grab it.'), nl.
+  look_armor :- write('Feeling insecure? Just grab that armor and you feel free.'), nl.
+  look_medicine :- write('Give up? Never say that! Claim your victor. Take that medicine and kill all your enemy!'), nl.
+  north_deadzone :- write('One step to the north and you will die.'), nl.
+  south_deadzone :- write('One step to the south and you will die.'), nl.
+  west_deadzone :- write('One step to the west and you will die.'), nl.
+  east_deadzone :- write('One step to the east and you will die.'), nl.
+  northeast_deadzone :- write('One step to the north or east and you will die.'), nl.
+  northwest_deadzone :- write('One step to the north or west and you will die.'), nl.
+  southeast_deadzone :- write('One step to the south or east and you will die.'), nl.
+  southwest_deadzone :- write('One step to the south or west and you will die.'), nl.
+  free_zone :- write('You see nothing. You're feeling lonely right now. Maybe you already become a victor?'), nl.
+  empty_ammo :- write('Shame on you. Weapon without ammo, huh?'), nl.
+  attack_enemy :- write('It's time. Grab your weapon. Shoot him! BOOM!'), nl.
+  deadzone_expanded :- write('OH NO! Deadzone is expanding. Be careful.'), nl.
